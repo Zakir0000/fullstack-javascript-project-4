d5bc26d0ee39491ebfc13ecaf934927bdc1a47e2
Zakir0000 <zakirkhunkaev@gmail.com>
2025-04-04 00:50:16 +0300

    fix commander file

--- page-loader.js
+++ page-loader.js
@@ -4,7 +4,7 @@ import { program } from 'commander';
 import downloadPage from './utilities/downLoadPage.js';
 import debug from 'debug';
 
-const log = debug('hexlet:page-loader');
+const log = debug('page-loader');
 
 program
   .name('page-loader')
@@ -14,7 +14,17 @@ program
   .option('-o, --output <dir>', 'Output directory', process.cwd())
   .action((url, options) => {
     log(`Running page-loader for ${url} with output directory: ${options.output}`);
-    downloadPage(url, options.output);
+    return downloadPage(url, options.output);
   });
 
-program.parse();
+// Only run the CLI parser if this file is executed directly.
+if (process.argv[1] === new URL(import.meta.url).pathname) {
+  program.exitOverride();
+  program.parseAsync().catch(err => {
+    // Print the error and exit with the specified exit code.
+    console.error(err);
+    process.exit(err.exitCode || 1);
+  });
+}
+
+export default downloadPage;
