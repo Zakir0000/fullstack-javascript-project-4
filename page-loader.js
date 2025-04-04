13245df2933f59d3bda8043fdee02a346a686635
Zakir0000 <zakirkhunkaev@gmail.com>
2025-04-04 03:48:01 +0300

    exec p to func

--- page-loader.js
+++ page-loader.js
@@ -6,15 +6,26 @@ import debug from 'debug';
 
 const log = debug('hexlet:page-loader');
 
-program
-  .name('page-loader')
-  .description('Page loader utility')
-  .version('1.0.0')
-  .argument('<url>', 'URL страницы для загрузки')
-  .option('-o, --output <dir>', 'Output directory', process.cwd())
-  .action((url, options) => {
-    log(`Running page-loader for ${url} with output directory: ${options.output}`);
-    downloadPage(url, options.output);
-  });
+// Main function to run the program
+async function run() {
+  try {
+    program
+      .name('page-loader')
+      .description('Page loader utility')
+      .version('1.0.0')
+      .argument('<url>', 'URL страницы для загрузки')
+      .option('-o, --output <dir>', 'Output directory', process.cwd())
+      .action(async (url, options) => {
+        log(`Running page-loader for ${url} with output directory: ${options.output}`);
+        await downloadPage(url, options.output); // Assuming downloadPage is async
+      });
 
-program.parse();
+    program.parse();
+  } catch (error) {
+    console.error('An error occurred:', error.message);
+    process.exit(1); // Exit with a non-zero status code in case of an error
+  }
+}
+
+// Call the main function
+run();
